37f8404622c776b122af3e8b1fc87542e3a8528c
alexrobaina <alexrobainaph@gmail.com>
2020-01-24 18:53:14 -0300

    navbar working progress

--- src/components/ListPets/ListPets.jsx
+++ src/components/ListPets/ListPets.jsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react'
-import { observer } from 'mobx-react'
 import URL_LOCAL from '../../config/config'
-import Navbar from '../commons/Navbar/Navbar'
+import LazyLoad from 'react-lazyload'
+import c from 'classnames'
+import { Animated } from 'react-animated-css'
 import Card from '../commons/Card/Card'
 import { MdCancel } from 'react-icons/md'
 import styles from './listPets.module.scss'
@@ -10,28 +11,33 @@ const ListPets = ({ filters, pets, isLoading, handleDelete }) => {
   return (
     <Fragment>
       <div className={styles.containerFilters}>
-        {filters && filters.map(filter => (
-          <div onClick={() => handleDelete(filter.text)} className={styles.filter}>
-            {filter.text}
-            <span className={styles.icons}>
-              <MdCancel size={16} />
-            </span>
-          </div>
-        ))}
+        {filters &&
+          filters.map(filter => (
+            <div onClick={() => handleDelete(filter.text)} className={styles.filter}>
+              {filter.text}
+              <span className={styles.icons}>
+                <MdCancel size={16} />
+              </span>
+            </div>
+          ))}
       </div>
       <div className={styles.container}>
         {isLoading
           ? pets.map(pet => {
               return (
-                <Card
-                  className={styles.card}
-                  key={pet._id}
-                  image={`${URL_LOCAL}${pet.image.filename}`}
-                  namePet={pet.name}
-                  history={
-                    'Inmediatamente aparecerá el instalador y se iniciará VirtualBox. Si tenemos un pack más antiguo instalado nos notificará si queremos actualizarlo al más recientemente descargado.'
-                  }
-                />
+                <LazyLoad height={200} offset={100}>
+                  <Animated animationIn="bounceInUp" animationOut="fadeOut" isVisible={true} animationInDuration={2000} >
+                    <Card
+                      className={styles.card}
+                      key={pet._id}
+                      image={`${URL_LOCAL}${pet.image.filename}`}
+                      namePet={pet.name}
+                      history={
+                        'Inmediatamente aparecerá el instalador y se iniciará VirtualBox. Si tenemos un pack más antiguo instalado nos notificará si queremos actualizarlo al más recientemente descargado.'
+                      }
+                    />
+                  </Animated>
+                </LazyLoad>
               )
             })
           : 'loading... '}
--- src/components/commons/Navbar/Navbar.jsx
+++ src/components/commons/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment, useState, useCallback, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import user from './anton-darius-thesollers-LH-NYOZmENI-unsplash.jpg'
-import menuIcon from './sliders-h-solid.svg'
+import { FiFilter } from 'react-icons/fi'
+import { MdClose } from 'react-icons/md'
 import c from 'classnames'
+import { Animated } from 'react-animated-css'
 import anime from 'animejs'
-import shapeIcon from './Shape.svg'
+import user from './anton-darius-thesollers-LH-NYOZmENI-unsplash.jpg'
 import styles from './navbar.module.scss'
 
 const Navbar = ({ timeAnimation }) => {
@@ -25,45 +26,39 @@ const Navbar = ({ timeAnimation }) => {
   }, [])
 
   return (
-    <div className={c(styles.animationOpacity, 'animationTitle')}>
-      {toggle === false ? (
-        <div className={styles.container}>
-          <div className={styles.menu} onClick={handleToggle}>
-            {/* change icon for filter */}
-            <img className={styles.menuImage} src={shapeIcon} alt="user" />
-          </div>
-          {/* <div className={styles.menu1}>
-            <img className={styles.menuImage1} src={menuIcon} alt="user" />
-          </div> */}
-          <div className={styles.userContainer}>
-            <img className={styles.userImage} src={user} alt="user" />
-          </div>
-        </div>
-      ) : (
-        <Fragment>
-          <div className={styles.container}>
+    <div>
+      {toggle ? (
+        <Animated animationIn="bounceInRight" isVisible={true} animationInDuration={2000}>
+          <div className={c(styles.container, 'animationTitle', styles.animationTitle)}>
             <div className={styles.menuOpen}>
               <div className={styles.menuClose} onClick={handleToggle}>
-                {/* change icon for back */}
-                <img className={styles.menuImage} src={shapeIcon} alt="user" />
+                <MdClose size={25} />
               </div>
             </div>
-            {/* <div className={styles.menu1}>
-              <img className={styles.menuImage1} src={menuIcon} alt="user" />
-            </div> */}
+            <div className={styles.userContainer}>
+              <img className={styles.userImage} src={user} alt="user" />
+            </div>
+            <div className={styles.shadowBack} onClick={handleToggle}></div>
+          </div>
+        </Animated>
+      ) : (
+        <div className={styles.container}>
+          <Animated animationIn="bounceInRigt" isVisible={true} animationInDuration={2000}>
+            <div className={styles.menu} onClick={handleToggle}>
+              <FiFilter size={25} />
+            </div>
+        </Animated>
             <div className={styles.userContainer}>
               <img className={styles.userImage} src={user} alt="user" />
             </div>
           </div>
-          <div className={styles.shadowBack} onClick={handleToggle}></div>
-        </Fragment>
       )}
     </div>
   )
 }
 
 Navbar.propTypes = {
-  timeAnimation: PropTypes.number.isRequired
+  timeAnimation: PropTypes.number.isRequired,
 }
 
 export default Navbar
