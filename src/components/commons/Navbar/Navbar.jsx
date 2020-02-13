9a69148f54967f22fa6f9c8736be21310a08fb84
alexrobaina <alexrobainaph@gmail.com>
2020-02-13 14:53:29 -0300

    refactor code

--- src/components/commons/Navbar/Navbar.jsx
+++ src/components/commons/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { FiFilter } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
 import { useHistory } from 'react-router'
 import { MdClose } from 'react-icons/md'
 import c from 'classnames'
@@ -10,12 +9,12 @@ import user from './anton-darius-thesollers-LH-NYOZmENI-unsplash.jpg'
 import noImg from './noimg.png'
 import styles from './navbar.scss'
 
-const Navbar = ({ searchPetsStore, optionsSelectsStore }) => {
+const Navbar = ({ searchPetsStore, optionsSelectsStore, isUserLogin }) => {
   const history = useHistory()
   const [toggle, setToggle] = useState(false)
-  const isUserLogin = true
 
   const goToLogin = useCallback(() => history.push('/login'), [])
+
   const goToRegister = useCallback(() => history.push('/register'), [])
 
   const handleToggle = useCallback(() => {
@@ -39,7 +38,7 @@ const Navbar = ({ searchPetsStore, optionsSelectsStore }) => {
           </div>
         ) : (
           <div>
-            <img className={styles.userImage} src={user ? user : noImg} alt="user" />
+            <img className={styles.userImage} src={isUserLogin ? user : noImg} alt="user" />
           </div>
         )}
       </div>
@@ -65,5 +64,11 @@ const Navbar = ({ searchPetsStore, optionsSelectsStore }) => {
 Navbar.propTypes = {
   searchPetsStore: PropTypes.node.isRequired,
   optionsSelectsStore: PropTypes.node.isRequired,
+  isUserLogin: PropTypes.bool,
 }
+
+Navbar.defaultProps = {
+  isUserLogin: true,
+}
+
 export default Navbar
