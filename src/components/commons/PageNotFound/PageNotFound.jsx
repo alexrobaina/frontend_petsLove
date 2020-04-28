b89173c796ffc936f7b8210fb8126cfec10274bb
davidciliberto1 <davidciliberto@gmail.com>
2020-04-28 13:54:15 -0300

    Page not found

--- src/components/commons/PageNotFound/PageNotFound.jsx
+++ src/components/commons/PageNotFound/PageNotFound.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import LayoutContainer from 'components/commons/LayoutContainer'
 import Navbar from 'components/commons/Navbar'
+import imagePNF from './ImagePNF.png'
+import styles from './pageNotFound.scss'
 
-// a las propiedades no les des vola todavia!! Las agregamos depsues
+// a las propiedades no les des vola todavia!! Las agregamos despues
 const PageNotFound = () => {
   return (
     <Navbar>
-      <LayoutContainer>{/*Aqui va el codigo que tienes que desarrollar*/}</LayoutContainer>
+      <LayoutContainer>
+        <div className={styles.pageNotFound}>
+          <img className={styles.imagePNF} src={imagePNF} alt="Not Found" />
+          <h2 className={styles.textPNF}>Page Not Found</h2>
+        </div>
+      </LayoutContainer>
     </Navbar>
   )
 }
