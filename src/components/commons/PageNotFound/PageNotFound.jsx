73fb9f23835b3662d558a7a7d8ac79494deefb14
alexrobaina <alexrobainaph@gmail.com>
2020-04-29 20:09:42 -0300

    Create new features and refactore code.

--- src/components/commons/PageNotFound/PageNotFound.jsx
+++ src/components/commons/PageNotFound/PageNotFound.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import LayoutContainer from 'components/commons/LayoutContainer'
-import Navbar from 'components/commons/Navbar'
 import imagePNF from './ImagePNF.png'
 import styles from './pageNotFound.scss'
 
-// a las propiedades no les des vola todavia!! Las agregamos despues
 const PageNotFound = () => {
   return (
-    <Navbar>
-      <LayoutContainer>
-        <div className={styles.pageNotFound}>
-          <img className={styles.imagePNF} src={imagePNF} alt="Not Found" />
-          <h2 className={styles.textPNF}>Page Not Found</h2>
-        </div>
-      </LayoutContainer>
-    </Navbar>
+    <LayoutContainer>
+      <div className={styles.pageNotFound}>
+        <img className={styles.imagePNF} src={imagePNF} alt="Not Found" />
+        <h2 className={styles.textPNF}>Page Not Found</h2>
+      </div>
+    </LayoutContainer>
   )
 }
 
