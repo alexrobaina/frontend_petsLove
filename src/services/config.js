8a75928e064c69206ae535ddc99c20020e68bd7f
alexrobaina <alexrobainaph@gmail.com>
2020-05-10 18:19:57 -0300

    change config js

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,10 @@
 const API_URL = 'https://private-2034fc-petslove.apiary-mock.com'
 // const API_MONGO_LOCAL = 'http://localhost:3000'
-// const SERVER = 'https://petsloveapp.herokuapp.com'
-// const HOST = 'https://petsloveapp.herokuapp.com'
+const SERVER = 'https://petsloveapp.herokuapp.com'
+const HOST = 'https://petslovefontend.herokuapp.com'
 // const HOST = 'https://pets-love.app'
-const SERVER = 'http://localhost:3000'
-const HOST = 'http://localhost:3001'
+// const SERVER = 'http://localhost:3000'
+// const HOST = 'http://localhost:3001'
 
 export {
   HOST,
