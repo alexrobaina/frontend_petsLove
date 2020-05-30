b8057481cb1f6166e8c1ea176d68a0c2effef172
alexrobaina <alexrobainaph@gmail.com>
2020-05-30 19:15:54 -0300

    setting server and host

--- src/services/config.js
+++ src/services/config.js
@@ -1,16 +1,4 @@
-const API_URL = 'https://private-2034fc-petslove.apiary-mock.com'
-// const API_MONGO_LOCAL = 'http://localhost:3000'
-const SERVER = 'http://localhost:3000'
-// const SERVER = process.env.REACT_APP_SERVER
-// const HOST = process.env.REACT_APP_HOST
-// const HOST = 'https://petslovefontend.herokuapp.com'
-// const SERVER = 'https://petsloveapp.herokuapp.com'
-// const HOST = 'https://pets-love.app'
-const HOST = 'http://localhost:3001'
+const SERVER = process.env.REACT_APP_SERVER
+const HOST = process.env.REACT_APP_HOST
 
-export {
-  HOST,
-  API_URL,
-  // API_MONGO_LOCAL,
-  SERVER,
-}
+export { HOST, SERVER }
