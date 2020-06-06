515bd0ecbe1e3d5b68220d03aaebe94b31121e23
alexrobaina <alexrobainaph@gmail.com>
2020-06-05 21:01:29 -0300

    hot fix image users

--- src/containers/ProfileUser/ProtectionistProfile/ProtectionistProfile.jsx
+++ src/containers/ProfileUser/ProtectionistProfile/ProtectionistProfile.jsx
@@ -37,7 +37,10 @@ const ProtectionistProfile = ({ user }) => {
   return (
     <LayoutContainer>
       <div className={styles.containerTitle}>
-        <Title rolText={t('protectionistUser.role')} title={t('protectionistUser.titleNameUser', { name })} />
+        <Title
+          rolText={t('protectionistUser.role')}
+          title={t('protectionistUser.titleNameUser', { name })}
+        />
         <ButtonsPet email={email} phone={phone} image={image} />
       </div>
       <div className={c(styles.containerCard, styles.layourCard)}>
