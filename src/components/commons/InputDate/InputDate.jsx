760e694d40447c554f33708c1855697e22c23b04
alexrobaina <alexrobainaph@gmail.com>
2020-07-10 11:24:17 -0300

    hot fix

--- src/components/commons/InputDate/InputDate.jsx
+++ src/components/commons/InputDate/InputDate.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { observer } from 'mobx-react'
+import c from 'classnames'
 import PropTypes from 'prop-types'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import './customDataPicker.css'
 import Label from '../Label/Input'
 import styles from './inputDate.scss'
 
 const InputDate = ({ label, handleDateChange, value }) => {
   const [selectedDate, handleDate] = useState(new Date())
-  
-  
+
   const CustomInput = React.forwardRef((props, ref) => (
     <button ref={ref} type="button" className={styles.input} onClick={props.onClick}>
       {props.value}
@@ -18,9 +19,9 @@ const InputDate = ({ label, handleDateChange, value }) => {
   useEffect(() => {
     handleDateChange(selectedDate)
   }, [selectedDate])
-  
+
   return (
-    <div className={styles.containerDate}>
+    <div className={c(styles.containerDate, 'customDatePickerWidth')}>
       {label && <Label text={label} />}
       <DatePicker
         value={value}
