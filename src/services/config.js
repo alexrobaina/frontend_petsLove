20e61c7184bc58d13a809b30f279d4921f3a7e51
alexrobaina <alexrobainaph@gmail.com>
2020-07-30 13:42:08 -0300

    change config services

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,4 @@
-const AWS_STORAGE = ''
-// const AWS_STORAGE = process.env.REACT_APP_AWS_STORAGE
+const AWS_STORAGE = process.env.REACT_APP_AWS_STORAGE
 const SERVER = process.env.REACT_APP_SERVER
 const HOST = process.env.REACT_APP_HOST
 
