c88d7bcd4a1aef8d79f6e8ed33ce1c36c8cdc98d
alexrobaina <alexrobainaph@gmail.com>
2020-08-02 20:42:40 -0300

    add page not found

--- src/components/commons/PageNotFound/PageNotFound.jsx
+++ src/components/commons/PageNotFound/PageNotFound.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import LayoutContainer from 'components/commons/LayoutContainer'
-import imagePNF from './ImagePNF.png'
+import imagePNF from './pageNotFound.png'
 import styles from './pageNotFound.scss'
 
 const PageNotFound = () => {
   return (
-    <LayoutContainer>
-      <div className={styles.pageNotFound}>
-        <img className={styles.imagePNF} src={imagePNF} alt="Not Found" />
-        <h2 className={styles.textPNF}>Page Not Found</h2>
-      </div>
-    </LayoutContainer>
+    <div className={styles.pageNotFound}>
+      <img className={styles.imagePNF} src={imagePNF} alt="Not Found" />
+    </div>
   )
 }
 
