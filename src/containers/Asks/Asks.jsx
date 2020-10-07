7de18b90ba944c698b7ea56c91029dfb8418fb2a
EANimesha <nimeshadilini999@gmail.com>
2020-10-07 21:51:43 +0530

    added json data

--- src/containers/Asks/Asks.jsx
+++ src/containers/Asks/Asks.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import imageAsks from './shelter.jpg'
+import Footer from 'components/commons/Footer/index'
+import Chips from 'components/commons/Chips/Chips'
 import styles from './asks.scss'
 import QuestionAndAnswer from './QuestionAndAnswer/index'
+import data from '../../services/AsksService/data.json'
+import imageAsks from './shelter.jpg'
 
 const Asks = () => {
     return( 
       <div className={styles.asksPage}>
         <img className={styles.imagePNF} src={imageAsks} alt="Not Found" />
-        <div className={styles.mainTitle}>heloop </div>
+        <div className={styles.mainTitle}> </div>
         <div className={styles.container}>
-          <QuestionAndAnswer question="this is a question?" answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-          <QuestionAndAnswer question="this is a question?" answer="this is an answer" />
-          <QuestionAndAnswer question="this is a question?" answer="this is an answer" />
+          {data.map(qAndA => {
+                      return (
+                        <QuestionAndAnswer question={qAndA.question} answer={qAndA.answer} />
+                      );
+            })}
         </div>
+        <Chips />
+        <Footer />
       </div>
     )
 }
