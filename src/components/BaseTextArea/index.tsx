3c134475a6b8182ed329daef86fd67dfb58cfe09
usrj03l <6rj1598@gmail.com>
2023-09-27 11:53:08 +0530

    updated BaseTextArea to arrow function

--- src/components/BaseTextArea/index.tsx
+++ src/components/BaseTextArea/index.tsx
@@ -1,32 +1,37 @@
+import { FC, ChangeEvent } from 'react'
+
 interface props {
-  Value?: string
+  value?: string
   className?: string
   height?: string | number
   textHelper?: string
   textError?: string
-  handleChange: (value: string) => void
+  handleChange: (e: ChangeEvent) => void
 }
 
-export default function BaseTextArea(Props: props) {
+export const BaseTextArea: FC<props> = ({
+  value,
+  className,
+  height,
+  textHelper,
+  textError,
+  handleChange,
+}) => {
   const styles =
     ' resize rounded-md border-0 py-1.5 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 outline-none placeholder:text-neutral-400 focus:ring-primary-500 sm:text-sm sm:leading-6 pl-4'
 
   return (
     <>
       <textarea
-        onChange={(e) => Props.handleChange(e.target.value)}
-        value={Props.Value}
-        className={`${Props.className} 
-            h-[${Props.height}px] 
+        onChange={handleChange}
+        value={value}
+        style={{ height: `${height}px` }}
+        className={`${className} 
             ${styles} 
-            ${Props.textError ? 'ring-red-500' : ''}`}
-      ></textarea>
-      {Props.textHelper && !Props.textError && (
-        <p className="pl-3 pt-3">* {Props.textHelper}</p>
-      )}
-      {Props.textError && (
-        <p className="pl-3 pt-3 text-red-500">* {Props.textError}</p>
-      )}
+            ${textError ? 'ring-red-500' : ''}`}
+      />
+      {textHelper && !textError && <p className="pl-3 pt-3">* {textHelper}</p>}
+      {textError && <p className="pl-3 pt-3 text-red-500">* {textError}</p>}
     </>
   )
 }
